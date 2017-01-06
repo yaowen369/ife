ab2a65f1a54b8ebd60fd9c3b300c003975fd558d
yaowen <yaowen369@163.com>
2017-01-06 22:30:23 +0800

    task2_13完成，初入接触js

--- 2016_spring/test/liaoxuefeng/first_demo/first_js.js
+++ 2016_spring/test/liaoxuefeng/first_demo/first_js.js
@@ -5,13 +5,13 @@ var a = ['A', 'B', 'C'];
 var s = new Set(['A', 'B', 'C']);
 var m = new Map([[1, 'x'], [2, 'y'], [3, 'z']]);
 for (var x of a) { // 遍历Array
-    alert(x);
+//  alert(x);
 }
 for (var x of s) { // 遍历Set
-    alert(x);
+//  alert(x);
 }
 for (var x of m) { // 遍历Map
-    alert(x[0] + '=' + x[1]);
+//  alert(x[0] + '=' + x[1]);
 }
 
 
