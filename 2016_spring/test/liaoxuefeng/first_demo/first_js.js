fdca59844faa55ab918c70d2258267822f0ac5ee
yaowen <yaowen369@163.com>
2017-03-02 22:39:20 +0800

    js继承的联系

--- 2016_spring/test/liaoxuefeng/first_demo/first_js.js
+++ 2016_spring/test/liaoxuefeng/first_demo/first_js.js
@@ -2,59 +2,146 @@
 
 
 var liaoTest = {
-	xiaoming : {
-		name : 'xiaoming',
-		birth : 1990,
-		middle_school : "No.1 Middle School",
-		print : function () {
-			
-        }
-	},
-
-	print : function () {
-		var str = "";
-		if (arguments.length > 0){
-			str += arguments[0];
-		}
-
-		str += "  name = ";
-		str += this.xiaoming.name;
-		str += "\t schopl = "
-		str += this.xiaoming.middle_school;
-
-		str += "\t score = ";
-		str += this.xiaoming.score;
-
-		console.log(str);
-    },
+    // xiaoming : {
+		// name : 'xiaoming',
+		// birth : 1990,
+		// middle_school : "No.1 Middle School",
+		// print : function () {
+		//
+    //     }
+    // },
+    //
+    // print : function () {
+		// var str = "";
+		// if (arguments.length > 0){
+		// 	str += arguments[0];
+		// }
+    //
+		// str += "  name = ";
+		// str += this.xiaoming.name;
+		// str += "\t schopl = "
+		// str += this.xiaoming.middle_school;
+    //
+		// str += "\t score = ";
+		// str += this.xiaoming.score;
+    //
+		// console.log(str);
+    // },
 
     test : function () {
-        console.log('middle_school' in this.xiaoming);
-        console.log('toString' in this.xiaoming);
-        console.log('print' in this.xiaoming);
-		// this.print("before delete");
+
+		var Student = {
+			name : 'Robot',
+			height : 1.2,
+			run : function () {
+				console.log(this.name + ' is running');
+            }
+		};
+
+		var Bird = {
+			fly : function () {
+				console.log(this.name + " is flying");
+            }
+		}
+
+		function createStudne(name) {
+			var s = Object.create(Student);
+			s.name = name;
+			return s;
+        }
+
+        var xiaoming = createStudne("xiao_ming");
+		xiaoming.run();
+		console.log(xiaoming.__proto__ === Student);
+
+        // var xiaoming = {
+			// name : 'xiaoming'
+        // };
         //
+        // xiaoming.__proto__ = Student;
         //
-		// delete this.xiaoming.primary_school;
-		// delete this.xiaoming.middle_school;
-		// this.print("after delete");
+        // console.log(xiaoming.name);
+        // xiaoming.run();
+        // console.log("------------1----------------");
+        // xiaoming.__proto__ = Bird;
+        // // xiaoming.run();
+        // console.log("------------2----------------");
+        // xiaoming.fly();
+    }, //end of test1
 
-		// console.log('middle_school' in this.xiaoming);
+	test2:function () {
 
-		var result_1 = this.xiaoming.hasOwnProperty("middle_school");
-		var result_2 = this.xiaoming.hasOwnProperty("toString");
-		var result_3 = this.xiaoming.hasOwnProperty("print");
-		console.log(result_1);
-		console.log(result_2);
-		console.log(result_3);
+		function Student(name) {
+			this.name = name;
+        }
+        
+        Student.prototype.hello = function () {
+			console.log("hello2, " + this.name + "!");
+        }
 
-    }
+        var xiaoming = new Student("xiaoMing");
+		// console.log(xiaoming.name);
+		// xiaoming.hello();
+
+		var xiaojun = new Student("xiaoJun");
+
+		var result = "result_1=";
+		result += (xiaoming.constructor === Student.prototype.constructor);
+		result += "\t result_2=";
+		result += (Student.prototype.constructor === Student);
+		result += "\t result_3=";
+		result += (Object.getPrototypeOf(xiaoming) === Student.prototype);
+		result += "\t result_4=";
+		result += (xiaoming instanceof Student);
+
+		var secondResult = "1=";
+		secondResult += (xiaoming.hello === xiaojun.hello);
+		secondResult += "\t 2=";
+		secondResult += (xiaoming === xiaojun);
+		secondResult += "\t 3=";
+		// secondResult += (xiaoming instanceof  xiaojun);
+
+		console.log(result);
+		console.log(secondResult);
+    }//end of "test2:"
 };
 
 
-window.onload = liaoTest.test();
+window.onload = liaoTest.test2();
 
 
+//
+// function Student(name) {
+//     this.name = name;
+//     this.hello = function () {
+//         console.log("Hello, " + this.name + "!");
+//     }
+// }
+//
+// var xiaoming = new Student("xiaoMing");
+// // console.log(xiaoming.name);
+// // xiaoming.hello();
+//
+// var xiaojun = new Student("xiaoJun");
+//
+// var result = "result_1=";
+// result += (xiaoming.constructor === Student.prototype.constructor);
+// result += "\t result_2=";
+// result += (Student.prototype.constructor === Student);
+// result += "\t result_3=";
+// result += (Object.getPrototypeOf(xiaoming) === Student.prototype);
+// result += "\t result_4=";
+// result += (xiaoming instanceof Student);
+//
+// var secondResult = "11=";
+// secondResult += (xiaoming.hello === xiaojun.hello);
+// secondResult += "\t 22=";
+// secondResult += (xiaoming === xiaojun);
+// secondResult += "\t 33=";
+// // secondResult += (xiaoming instanceof  xiaojun);
+//
+// // console.log(result);
+// console.log(secondResult);
 
 
 
