261037ba4c83af8c8dc53aac8030b515e1ec4587
yaowen <yaowen369@163.com>
2017-03-16 07:34:51 +0800

    task2_20

--- 2016_spring/task2_20/src/task_20.js
+++ 2016_spring/task2_20/src/task_20.js
@@ -6,5 +6,29 @@
 window.onload = task20();
 
 function task20() {
-    console.log("enter task20");
+
+    run();
+
+    //执行的总方法
+    function run() {
+        document.getElementById("insert-btn").addEventListener("click", insertBtnClick);
+        document.getElementById("queryBtn").addEventListener("click", queryBtnClick);
+    };
+
+
+
+
+    function insertBtnClick() {
+        var inputStr = document.getElementsByTagName("textarea")[0].nodeValue;
+        console.log("inputStr = " + inputStr);
+    }
+
+    function queryBtnClick() {
+        var queryStr = document.getElementsByTagName("input")[2].nodeValue;
+        console.log("queryStr = " + queryStr);
+    }
+
+
+
+
 };
