8441ab41dc62011d11fd9840d90b74ec5082a8b2
yaowen <yaowen369@163.com>
2017-05-02 22:49:00 +0800

    增加task2_21

---
+++ 2016_spring/task2_21/src/task21.js
@@ -0,0 +1,21 @@
+/**
+ * Created by yw on 17/4/25.
+ */
+
+window.onload = function () {
+
+    run();
+
+    function run() {
+
+    };
+
+    //用链表实现一个队列，里面最多保存10个不重复的元素
+    function LinkedList() {
+        var Node = function (element) {
+            this.element = element;
+            this.next = null;
+        }
+    }
+   
+};
